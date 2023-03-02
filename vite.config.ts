2ef1d3a56b6b4bd204695f9d7ef65577a3ec9d97
James Talton <jtalton@redhat.com>
2023-03-02 10:28:43 -0500

    vite
    Signed-off-by: James Talton <jtalton@redhat.com>

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+/// <reference types="vitest" />
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: './tests/setup.ts',
+  },
+})
