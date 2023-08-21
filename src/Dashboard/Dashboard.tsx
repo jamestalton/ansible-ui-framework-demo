fc90593a98fba5127f53034fdcfc580830321a5e
James Talton <talton@gmail.com>
2023-08-21 13:53:45 -0400

    dashboard
    Signed-off-by: James Talton <talton@gmail.com>

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -1,24 +1,61 @@
 import {
   PageDashboard,
+  PageDashboardCard,
+  PageDashboardChart,
   PageDashboardDonutCard,
   PageHeader,
   pfSuccess,
 } from '@ansible/ansible-ui-framework'
+import { useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
-import { PageDashboardCountCard } from '../PageDashboardCountCard'
 import { useIdbItems } from '../common/IDBProvider'
 
 export function Dashboard() {
+  const users = useIdbItems('users')
+
+  // chart data for last 30 days when user was created
+  const userData: {
+    color: string
+    values: {
+      label: string
+      value: number
+    }[]
+  }[] = useMemo(() => {
+    const data: {
+      color: string
+      values: {
+        label: string
+        value: number
+      }[]
+    }[] = []
+    data.push({
+      color: pfSuccess,
+      values: new Array(30)
+        .fill(0)
+        .map((_, i) => {
+          const date = new Date(Date.now() - i * 24 * 60 * 60 * 1000)
+          return {
+            label: date.toISOString().split('T')[0],
+            value:
+              users?.filter(
+                (user) => user.created?.split('T')[0] === date.toISOString().split('T')[0]
+              ).length ?? 1,
+          }
+        })
+        .reverse(),
+    })
+    return data
+  }, [users])
+
   return (
     <>
       <PageHeader title="Dashboard" />
       <PageDashboard>
         <UsersCard />
         <TeamsCard />
-      </PageDashboard>
-      <PageDashboard>
-        <UsersCountCard />
-        <TeamsCountCard />
+        <PageDashboardCard height="md" width="xxl" title="Users Created - Last 30 days">
+          <PageDashboardChart groups={userData} />
+        </PageDashboardCard>
       </PageDashboard>
     </>
   )
@@ -37,19 +74,6 @@ function UsersCard() {
   )
 }
 
-function UsersCountCard() {
-  const { t } = useTranslation()
-  const users = useIdbItems('users')
-  return (
-    <PageDashboardCountCard
-      title={t('Users')}
-      linkText={t('Go to Users')}
-      to="/access/users"
-      count={users?.length ?? 0}
-    />
-  )
-}
-
 function TeamsCard() {
   const { t } = useTranslation()
   const teams = useIdbItems('teams')
@@ -59,19 +83,6 @@ function TeamsCard() {
       linkText={t('Go to Teams')}
       to="/access/teams"
       items={[{ label: t('Teams'), count: teams?.length ?? 0, color: pfSuccess }]}
-    />
-  )
-}
-
-function TeamsCountCard() {
-  const { t } = useTranslation()
-  const teams = useIdbItems('teams')
-  return (
-    <PageDashboardCountCard
-      title={t('Teams')}
-      linkText={t('Go to Teams')}
-      to="/access/teams"
-      count={teams?.length ?? 0}
     />
   )
 }
